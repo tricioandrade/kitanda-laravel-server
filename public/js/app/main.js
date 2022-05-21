1f88a8fbfa8fc304152476d40c5e62f48ecdc8c9
Patrício Andrade <tricioandrade@outlook.com>
2022-05-21 01:00:22 +0100

    update

---
+++ public/js/app/main.js
@@ -0,0 +1,10 @@
+import React from "../react/umd/react.development.js";
+import ReactDOM from "../react-dom/umd/react-dom.development.js";
+import '../../css/app.css';
+import App from "./components/app.js";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
