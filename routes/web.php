ad861c661aec1d87fdf3750c25eca0c3339bf770
Patrício Andrade <tricioandrade@outlook.com>
2022-05-23 16:36:35 +0100

    rebuild

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get("/", [PagesController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
